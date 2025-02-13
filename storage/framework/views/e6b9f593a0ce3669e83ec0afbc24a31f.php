5bfbb8737343990ed57d57ba7e1efb46a48f2474
Bill <nabilfiras93@gmail.com>
2025-02-13 15:37:21 +0700

    cache view

---
+++ storage/framework/views/e6b9f593a0ce3669e83ec0afbc24a31f.php
@@ -0,0 +1,258 @@
+
+<?php $__env->startSection('title'); ?>
+    Monitoring Kamar Tidur Pasien
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('content'); ?>
+    
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <div class="flex-grow-1">
+                            <a data-bs-toggle="collapse" href="#collapseAnggrek" role="button" aria-expanded="false" class="collapseButton"><h6 class="card-title mb-0">Ruang Anggrek</h6></a>
+                        </div>
+                        <div class="flex-shrink-0">
+                            <ul class="list-inline card-toolbar-menu d-flex align-items-center mb-0">
+                                <li class="list-inline-item">
+                                    <a class="align-middle" href="javascript:void(0);" data-toggle="customer-loader">
+                                        <i class="mdi mdi-refresh align-middle"></i>
+                                    </a>
+                                </li>
+                                <!-- <li class="list-inline-item">
+                                    <a class="align-middle minimize-card" data-bs-toggle="collapse"
+                                        href="#collapseAnggrek" role="button" aria-expanded="false">
+                                        <i class="mdi mdi-plus align-middle plus"></i>
+                                        <i class="mdi mdi-minus align-middle minus"></i>
+                                    </a>
+                                </li> -->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <div class="flex-grow-1">
+                            <a data-bs-toggle="collapse" href="#collapseAnggrek" role="button" aria-expanded="false" class="collapseButton"><h6 class="card-title mb-0">Ruang Anggrek</h6></a>
+                        </div>
+                        <div class="flex-shrink-0">
+                            <ul class="list-inline card-toolbar-menu d-flex align-items-center mb-0">
+                                <li class="list-inline-item">
+                                    <a class="align-middle" href="javascript:void(0);" data-toggle="customer-loader">
+                                        <i class="mdi mdi-refresh align-middle"></i>
+                                    </a>
+                                </li>
+                                <!-- <li class="list-inline-item">
+                                    <a class="align-middle minimize-card" data-bs-toggle="collapse"
+                                        href="#collapseAnggrek" role="button" aria-expanded="false">
+                                        <i class="mdi mdi-plus align-middle plus"></i>
+                                        <i class="mdi mdi-minus align-middle minus"></i>
+                                    </a>
+                                </li> -->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body collapse show" id="collapseAnggrek">
+                    <div class="card">
+                        <div class="row">
+
+                            <div class="col-xxl-3 col-sm-6">
+                                <div class="card profile-project-card shadow-none profile-project-warning">
+                                    <div class="card-body p-4">
+                                        <div class="d-flex">
+                                            <div class="flex-grow-1 text-muted overflow-hidden">
+                                                <button type="button" class="mb-2 btn btn-danger  text-dark fw-bold">
+                                                Bed 1
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-success text-dark fw-bold">
+                                                Bed 2
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-success  text-dark fw-bold">
+                                                Bed 3
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-success text-dark fw-bold">
+                                                Bed 4
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-danger  text-dark fw-bold">
+                                                Bed 5
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-success text-dark fw-bold">
+                                                Bed 6
+                                                </button>
+                                            </div>
+                                            <div class="flex-shrink-0 ms-2">
+                                                <div class="badge bg-info-subtle text-dark fs-15">
+                                                    Kamar 1</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xxl-3 col-sm-6">
+                                <div class="card profile-project-card shadow-none profile-project-warning">
+                                    <div class="card-body p-4">
+                                        <div class="d-flex">
+                                            <div class="flex-grow-1 text-muted overflow-hidden">
+                                                <button type="button" class="mb-2 btn btn-success  text-dark fw-bold">
+                                                Bed 1
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-success text-dark fw-bold">
+                                                Bed 2
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-success  text-dark fw-bold">
+                                                Bed 3
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-success text-dark fw-bold">
+                                                Bed 4
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-danger  text-dark fw-bold">
+                                                Bed 5
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-success text-dark fw-bold">
+                                                Bed 6
+                                                </button>
+                                            </div>
+                                            <div class="flex-shrink-0 ms-2">
+                                                <div class="badge bg-info-subtle text-dark fs-15">
+                                                    Kamar 2</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xxl-3 col-sm-6">
+                                <div class="card profile-project-card shadow-none profile-project-warning">
+                                    <div class="card-body p-4">
+                                        <div class="d-flex">
+                                            <div class="flex-grow-1 text-muted overflow-hidden">
+                                                <button type="button" class="mb-2 btn btn-danger  text-dark fw-bold">
+                                                Bed 1
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-danger text-dark fw-bold">
+                                                Bed 2
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-success  text-dark fw-bold">
+                                                Bed 3
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-danger text-dark fw-bold">
+                                                Bed 4
+                                                </button>
+                                            </div>
+                                            <div class="flex-shrink-0 ms-2">
+                                                <div class="badge bg-info-subtle text-dark fs-15">
+                                                    Kamar 3</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xxl-3 col-sm-6">
+                                <div class="card profile-project-card shadow-none profile-project-warning">
+                                    <div class="card-body p-4">
+                                        <div class="d-flex">
+                                            <div class="flex-grow-1 text-muted overflow-hidden">
+                                                <button type="button" class="mb-2 btn btn-danger  text-dark fw-bold">
+                                                Bed 1
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-success text-dark fw-bold">
+                                                Bed 2
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-success  text-dark fw-bold">
+                                                Bed 3
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-danger text-dark fw-bold">
+                                                Bed 4
+                                                </button>
+                                                <button type="button" class="mb-2 btn btn-danger  text-dark fw-bold">
+                                                Bed 5
+                                                </button>
+                                          
+                                            </div>
+                                            <div class="flex-shrink-0 ms-2">
+                                                <div class="badge bg-info-subtle text-dark fs-15">
+                                                    Kamar 4</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <!-- <div class="col-lg-12">
+                                <div class="mt-4">
+                                    <ul class="pagination pagination-separated justify-content-center mb-0">
+                                        <li class="page-item disabled">
+                                            <a href="javascript:void(0);" class="page-link"><i
+                                                    class="mdi mdi-chevron-left"></i></a>
+                                        </li>
+                                        <li class="page-item active">
+                                            <a href="javascript:void(0);" class="page-link">1</a>
+                                        </li>
+                                        <li class="page-item">
+                                            <a href="javascript:void(0);" class="page-link">2</a>
+                                        </li>
+                                        <li class="page-item">
+                                            <a href="javascript:void(0);" class="page-link">3</a>
+                                        </li>
+                                        <li class="page-item">
+                                            <a href="javascript:void(0);" class="page-link">4</a>
+                                        </li>
+                                        <li class="page-item">
+                                            <a href="javascript:void(0);" class="page-link">5</a>
+                                        </li>
+                                        <li class="page-item">
+                                            <a href="javascript:void(0);" class="page-link"><i
+                                                    class="mdi mdi-chevron-right"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('script'); ?>
+<script type="text/javascript">
+    $(document).ready(function(){
+        checkCollapse()
+    });
+
+    const checkCollapse = async () => {
+        let collapse = ['collapseAnggrek'];
+        $.each(collapse, function(i, val){
+            if(localStorage.getItem(`#${val}`)){
+                $(`#${val}`).addClass('show');
+            } else {
+                $(`#${val}`).removeClass('show');
+            }
+        });
+    }
+
+    $(".collapseButton").click(function () {
+        let id = $(this).attr('href');
+        if(localStorage.getItem(id)){
+            localStorage.removeItem(id);
+        } else {
+            localStorage.setItem(id, 'true');
+        }
+    });
+</script>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.layouts-horizontal-only-topbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH G:\xampp82\htdocs\rsud\resources\views/bed/list.blade.php ENDPATH**/ ?>
